38f126c00c65a25de5759698e3cb8faed20d9469
Lakshay Sharma <LS035@atcltd.atc.org.nz>
2019-09-10 12:21:41 +1200

    Add project files.

---
+++ HenRace/Game.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace HenRace
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.hen4 = new System.Windows.Forms.PictureBox();
+            this.hen3 = new System.Windows.Forms.PictureBox();
+            this.hen2 = new System.Windows.Forms.PictureBox();
+            this.hen1 = new System.Windows.Forms.PictureBox();
+            this.henTrack = new System.Windows.Forms.PictureBox();
+            this.bettingParlor = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.arunBetLabel = new System.Windows.Forms.Label();
+            this.samBetLabel = new System.Windows.Forms.Label();
+            this.lakshayBetLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.Bets = new System.Windows.Forms.Button();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.arunRadioButton = new System.Windows.Forms.RadioButton();
+            this.samRadioButton = new System.Windows.Forms.RadioButton();
+            this.lakshayRadioButton = new System.Windows.Forms.RadioButton();
+            this.minimumBetLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.hen4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hen3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hen2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hen1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.henTrack)).BeginInit();
+            this.bettingParlor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hen4
+            // 
+            this.hen4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.hen4.Image = global::HenRace.Properties.Resources.hen_yellow;
+            this.hen4.Location = new System.Drawing.Point(27, 298);
+            this.hen4.Name = "hen4";
+            this.hen4.Size = new System.Drawing.Size(81, 50);
+            this.hen4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.hen4.TabIndex = 4;
+            this.hen4.TabStop = false;
+            // 
+            // hen3
+            // 
+            this.hen3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.hen3.Image = global::HenRace.Properties.Resources.hen_red;
+            this.hen3.Location = new System.Drawing.Point(27, 215);
+            this.hen3.Name = "hen3";
+            this.hen3.Size = new System.Drawing.Size(81, 48);
+            this.hen3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.hen3.TabIndex = 3;
+            this.hen3.TabStop = false;
+            // 
+            // hen2
+            // 
+            this.hen2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.hen2.Image = global::HenRace.Properties.Resources.hen_green;
+            this.hen2.Location = new System.Drawing.Point(27, 133);
+            this.hen2.Name = "hen2";
+            this.hen2.Size = new System.Drawing.Size(81, 50);
+            this.hen2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.hen2.TabIndex = 2;
+            this.hen2.TabStop = false;
+            // 
+            // hen1
+            // 
+            this.hen1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.hen1.Image = global::HenRace.Properties.Resources.hen_blue_;
+            this.hen1.Location = new System.Drawing.Point(27, 46);
+            this.hen1.Name = "hen1";
+            this.hen1.Size = new System.Drawing.Size(81, 50);
+            this.hen1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.hen1.TabIndex = 1;
+            this.hen1.TabStop = false;
+            // 
+            // henTrack
+            // 
+            this.henTrack.Image = global::HenRace.Properties.Resources.track;
+            this.henTrack.Location = new System.Drawing.Point(0, 0);
+            this.henTrack.Name = "henTrack";
+            this.henTrack.Size = new System.Drawing.Size(900, 400);
+            this.henTrack.TabIndex = 0;
+            this.henTrack.TabStop = false;
+            // 
+            // bettingParlor
+            // 
+            this.bettingParlor.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bettingParlor.Controls.Add(this.button1);
+            this.bettingParlor.Controls.Add(this.arunBetLabel);
+            this.bettingParlor.Controls.Add(this.samBetLabel);
+            this.bettingParlor.Controls.Add(this.lakshayBetLabel);
+            this.bettingParlor.Controls.Add(this.label2);
+            this.bettingParlor.Controls.Add(this.numericUpDown2);
+            this.bettingParlor.Controls.Add(this.label1);
+            this.bettingParlor.Controls.Add(this.numericUpDown1);
+            this.bettingParlor.Controls.Add(this.Bets);
+            this.bettingParlor.Controls.Add(this.nameLabel);
+            this.bettingParlor.Controls.Add(this.arunRadioButton);
+            this.bettingParlor.Controls.Add(this.samRadioButton);
+            this.bettingParlor.Controls.Add(this.lakshayRadioButton);
+            this.bettingParlor.Controls.Add(this.minimumBetLabel);
+            this.bettingParlor.Location = new System.Drawing.Point(12, 406);
+            this.bettingParlor.Name = "bettingParlor";
+            this.bettingParlor.Size = new System.Drawing.Size(879, 147);
+            this.bettingParlor.TabIndex = 7;
+            this.bettingParlor.TabStop = false;
+            this.bettingParlor.Text = "Betting Parlor";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkOrange;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(457, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(416, 113);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Race!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // arunBetLabel
+            // 
+            this.arunBetLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.arunBetLabel.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.arunBetLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.arunBetLabel.Location = new System.Drawing.Point(218, 85);
+            this.arunBetLabel.Name = "arunBetLabel";
+            this.arunBetLabel.Size = new System.Drawing.Size(233, 23);
+            this.arunBetLabel.TabIndex = 12;
+            this.arunBetLabel.Text = "ArunBetLabel";
+            // 
+            // samBetLabel
+            // 
+            this.samBetLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.samBetLabel.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.samBetLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.samBetLabel.Location = new System.Drawing.Point(218, 61);
+            this.samBetLabel.Name = "samBetLabel";
+            this.samBetLabel.Size = new System.Drawing.Size(233, 17);
+            this.samBetLabel.TabIndex = 11;
+            this.samBetLabel.Text = "SamBetLabel";
+            // 
+            // lakshayBetLabel
+            // 
+            this.lakshayBetLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lakshayBetLabel.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lakshayBetLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lakshayBetLabel.Location = new System.Drawing.Point(218, 38);
+            this.lakshayBetLabel.Name = "lakshayBetLabel";
+            this.lakshayBetLabel.Size = new System.Drawing.Size(233, 17);
+            this.lakshayBetLabel.TabIndex = 10;
+            this.lakshayBetLabel.Text = "GurPyarBetLabel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(175, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Bets";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(287, 112);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(49, 20);
+            this.numericUpDown2.TabIndex = 8;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(175, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "quids on car number";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(104, 112);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(65, 20);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Bets
+            // 
+            this.Bets.BackColor = System.Drawing.Color.Orange;
+            this.Bets.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Bets.Location = new System.Drawing.Point(49, 110);
+            this.Bets.Name = "Bets";
+            this.Bets.Size = new System.Drawing.Size(49, 23);
+            this.Bets.TabIndex = 5;
+            this.Bets.Text = "Bets";
+            this.Bets.UseVisualStyleBackColor = false;
+            this.Bets.Click += new System.EventHandler(this.Bets_Click);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(24, 115);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(19, 13);
+            this.nameLabel.TabIndex = 4;
+            this.nameLabel.Text = "----";
+            this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // arunRadioButton
+            // 
+            this.arunRadioButton.AutoSize = true;
+            this.arunRadioButton.BackColor = System.Drawing.Color.Tomato;
+            this.arunRadioButton.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.arunRadioButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.arunRadioButton.Location = new System.Drawing.Point(10, 85);
+            this.arunRadioButton.Name = "arunRadioButton";
+            this.arunRadioButton.Size = new System.Drawing.Size(159, 23);
+            this.arunRadioButton.TabIndex = 3;
+            this.arunRadioButton.TabStop = true;
+            this.arunRadioButton.Text = "ArunRadioButton";
+            this.arunRadioButton.UseVisualStyleBackColor = false;
+            // 
+            // samRadioButton
+            // 
+            this.samRadioButton.AutoSize = true;
+            this.samRadioButton.BackColor = System.Drawing.Color.Tomato;
+            this.samRadioButton.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.samRadioButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.samRadioButton.Location = new System.Drawing.Point(10, 61);
+            this.samRadioButton.Name = "samRadioButton";
+            this.samRadioButton.Size = new System.Drawing.Size(155, 23);
+            this.samRadioButton.TabIndex = 2;
+            this.samRadioButton.TabStop = true;
+            this.samRadioButton.Text = "SamRadioButton";
+            this.samRadioButton.UseVisualStyleBackColor = false;
+            // 
+            // lakshayRadioButton
+            // 
+            this.lakshayRadioButton.AutoSize = true;
+            this.lakshayRadioButton.BackColor = System.Drawing.Color.Tomato;
+            this.lakshayRadioButton.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lakshayRadioButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lakshayRadioButton.Location = new System.Drawing.Point(10, 37);
+            this.lakshayRadioButton.Name = "lakshayRadioButton";
+            this.lakshayRadioButton.Size = new System.Drawing.Size(183, 23);
+            this.lakshayRadioButton.TabIndex = 1;
+            this.lakshayRadioButton.Text = "GurPyarRadioButton";
+            this.lakshayRadioButton.UseVisualStyleBackColor = false;
+            // 
+            // minimumBetLabel
+            // 
+            this.minimumBetLabel.AutoSize = true;
+            this.minimumBetLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minimumBetLabel.Location = new System.Drawing.Point(7, 20);
+            this.minimumBetLabel.Name = "minimumBetLabel";
+            this.minimumBetLabel.Size = new System.Drawing.Size(104, 13);
+            this.minimumBetLabel.TabIndex = 0;
+            this.minimumBetLabel.Text = "minimumBetLabel";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 559);
+            this.Controls.Add(this.bettingParlor);
+            this.Controls.Add(this.hen4);
+            this.Controls.Add(this.hen3);
+            this.Controls.Add(this.hen2);
+            this.Controls.Add(this.hen1);
+            this.Controls.Add(this.henTrack);
+            this.Name = "Game";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.hen4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hen3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hen2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hen1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.henTrack)).EndInit();
+            this.bettingParlor.ResumeLayout(false);
+            this.bettingParlor.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox henTrack;
+        private System.Windows.Forms.PictureBox hen1;
+        private System.Windows.Forms.PictureBox hen2;
+        private System.Windows.Forms.PictureBox hen3;
+        private System.Windows.Forms.PictureBox hen4;
+        private System.Windows.Forms.GroupBox bettingParlor;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label arunBetLabel;
+        private System.Windows.Forms.Label samBetLabel;
+        private System.Windows.Forms.Label lakshayBetLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button Bets;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.RadioButton arunRadioButton;
+        private System.Windows.Forms.RadioButton samRadioButton;
+        private System.Windows.Forms.RadioButton lakshayRadioButton;
+        private System.Windows.Forms.Label minimumBetLabel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ HenRace/HenSetup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HenRace
+{
+    public class HenSetup : HenAbstractClass //inherited abstract class
+    {
+        public int HenStartingPosition; // where my PictureBox Starts
+        public int TrackLength; //How long the racetrack is
+        public PictureBox MyPictureBox = null; //My PictureBox object
+        public Random Randomizer; //An integer random
+        public int Loc = 0; //My Location on the track
+
+        public override bool HenRun()//This is overridden by abstract class
+        {
+            Loc += Randomizer.Next(1, 5);
+            MyPictureBox.Left = Loc;
+            if (MyPictureBox.Left >= TrackLength)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override void HenStartPosition()//This is overridden by abstract class
+        {
+            Loc = 0;
+            MyPictureBox.Left = HenStartingPosition;
+        }
+    }
+}
